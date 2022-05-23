7a0a0068cf616b9b34308e108e9abfc1c4bd4a80
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-23 09:57:45 -0700

    add user model

--- models/users.js
+++ models/users.js
@@ -1,14 +1,34 @@
-//username
-//String, Unique, Required, Trimmed
+const { Schema, model } = require('mongoose');
 
-//email
-//String, Required, Unique, valid email format
-
-//thoughts
-//array of _id referencing thoughts
-
-//friends
-//array of _id referencing user model (self-reference)
-
-//Virtual friendCount
-//count of friends
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Invalid Email Address']
+      },      
+      thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'thoughtSchema'
+      }],
+      friends: [{
+          type: Schema.Types.ObjectId,
+          ref: 'userSchema'
+        }]
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true
+      },
+      // prevents virtuals from creating duplicate of _id as `id`
+      id: false
+    }
+  );

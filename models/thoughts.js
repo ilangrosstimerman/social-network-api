39b1c4c92dafc965e2b2bcfad92196a0b3353ff1
ilantimerman <ilan.tmrmn@gmail.com>
2022-05-23 09:18:01 -0700

    add requirements for reference

---
+++ models/thoughts.js
@@ -0,0 +1,16 @@
+//thoughtText
+//String
+//Required
+//minLength 1 maxLength(280)
+
+//createdAt
+//Date
+//Set default to current time
+//use a getter method to format the timestamp on query
+
+//username
+//String
+//Required
+
+//reactions
+//Array of nested documents created with the reactionSchema
---
+++ models/users.js
@@ -0,0 +1,14 @@
+//username
+//String, Unique, Required, Trimmed
+
+//email
+//String, Required, Unique, valid email format
+
+//thoughts
+//array of _id referencing thoughts
+
+//friends
+//array of _id referencing user model (self-reference)
+
+//Virtual friendCount
+//count of friends
